5175fdaa29a2055a213e5b2b03b86378649aee2d
yourZaNoza <yourZaNoza@yandex.ru>
2024-09-12 14:05:13 +0300

    another commit

---
+++ СП_1/CП_ДЗ_2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // задание 1
+    int number = 42;
+    cout << number << endl;
+
+    // задание 2
+    double a = 3.14;
+    double b = 2.17;
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a * b << endl;
+    cout << a / b << endl;
+
+    // задание 3
+    char letter = 'A';
+    int code = int(letter);
+    cout << code << endl;
+
+    // задание 4
+    bool flag = true;
+    cout << boolalpha << flag << endl;
+    flag = false;
+    cout << boolalpha << flag << endl;
+
+    // задание 5
+    int value;
+    cout << "input value: " << endl;
+    cin >> value;
+    cout << (value % 2 == 0 ? "true" : "false") << endl;
+
+    // задание 6
+    int temp;
+    cout << "input celsius temperature: " << endl;
+    cin >> tamp;
+    cout << "fahrenheit temperature " << (temp * 9 / 5 + 32) << endl;
+
+    return 0;
+}
