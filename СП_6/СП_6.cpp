ecda3be934b93e35e7850c13406f387f783572ed
yourZaNoza <yourZaNoza@yandex.ru>
2024-09-29 14:32:19 +0300

    another commit

--- СП_6/СП_6.cpp
+++ СП_6/СП_6.cpp
@@ -178,80 +178,164 @@ using namespace std;
 //    return 0;
 //}
 // задание 7
-int compareStrings(const char* str1, const char* str2) {
-    while (*str1 != '\0' && *str2 != '\0') {
-        if (*str1 < *str2) {
-            return -1;
-        }
-        else if (*str1 > *str2) {
-            return 1;
-        }
-        str1++;
-        str2++;
+//int compareStrings(const char* str1, const char* str2) {
+//    while (*str1 != '\0' && *str2 != '\0') {
+//        if (*str1 < *str2) {
+//            return -1;
+//        }
+//        else if (*str1 > *str2) {
+//            return 1;
+//        }
+//        str1++;
+//        str2++;
+//    }
+//
+//    if (*str1 == '\0' && *str2 == '\0') {
+//        return 0;
+//    }
+//    else if (*str1 == '\0') {
+//        return -1;
+//    }
+//    else {
+//        return 1;
+//    }
+//}
+//
+//int main() {
+//    setlocale(LC_ALL, "");
+//    const char* str1 = "hello";
+//    const char* str2 = "hello";
+//    const char* str3 = "world";
+//    const char* str4 = "hell";
+//
+//    cout << "Сравниваем \"" << str1 << "\" и \"" << str2 << "\": " << compareStrings(str1, str2) << endl;
+//    cout << "Сравниваем \"" << str1 << "\" и \"" << str3 << "\": " << compareStrings(str1, str3) << endl;
+//    cout << "Сравниваем \"" << str3 << "\" и \"" << str1 << "\": " << compareStrings(str3, str1) << endl;
+//    cout << "Сравниваем \"" << str1 << "\" и \"" << str4 << "\": " << compareStrings(str1, str4) << endl;
+//    cout << "Сравниваем \"" << str4 << "\" и \"" << str1 << "\": " << compareStrings(str4, str1) << endl;
+//
+//    return 0;
+//}
+
+// задание 8
+//int* findCommonElements(const int* arr1, int size1, const int* arr2, int size2, int& commonSize) {
+//    int* commonElements = new int[size1 < size2 ? size1 : size2]; 
+//    commonSize = 0;
+//    for (int i = 0; i < size1; ++i) {
+//        for (int j = 0; j < size2; ++j) {
+//            if (arr1[i] == arr2[j]) {
+//                commonElements[commonSize++] = arr1[i];
+//                break; 
+//            }
+//        }
+//    }
+//    return commonElements;
+//}
+//
+//int main() {
+//    int arr1[] = { 1, 2, 3, 4, 5 };
+//    int size1 = sizeof(arr1) / sizeof(arr1[0]);
+//
+//    int arr2[] = { 3, 4, 5, 6, 7 };
+//    int size2 = sizeof(arr2) / sizeof(arr2[0]);
+//
+//    int commonSize;
+//    int* commonElements = findCommonElements(arr1, size1, arr2, size2, commonSize);
+//
+//    cout << "Похожие элементы: ";
+//    for (int i = 0; i < commonSize; ++i) {
+//        cout << commonElements[i] << " ";
+//    }
+//    cout << endl;
+//
+//    delete[] commonElements; 
+//
+//    return 0;
+//}
+// задание 9
+//void rotateArray(int* arr, int size, int positions) {
+//    positions = positions % size; 
+//    if (positions < 0) {
+//        positions += size; 
+//    }
+//    int* temp = new int[size];
+//    for (int i = 0; i < size; ++i) {
+//        temp[i] = arr[(i + positions) % size];
+//    }
+//    for (int i = 0; i < size; ++i) {
+//        arr[i] = temp[i];
+//    }
+//    delete[] temp; 
+//}
+//
+//int main() {
+//    setlocale(LC_ALL, "");
+//    int arr[] = { 1, 2, 3, 4, 5 };
+//    int size = sizeof(arr) / sizeof(arr[0]);
+//    int positions = 4;
+//
+//    cout << "Оригинальный массив: ";
+//    for (int i = 0; i < size; ++i) {
+//        cout << arr[i] << " ";
+//    }
+//    cout << endl;
+//
+//    rotateArray(arr, size, positions);
+//
+//    cout << "Перевернутый: ";
+//    for (int i = 0; i < size; ++i) {
+//        cout << arr[i] << " ";
+//    }
+//    cout << endl;
+//
+//    return 0;
+//}
+
+// 
+// задание 10
+
+int* mergeAndCompare(const int* arr1, int size1, const int* arr2, int size2, int& mergedSize) {
+    if (size1 != size2) {
+        mergedSize = -1; // указать на разные размеры
+        return nullptr; // указать на ошибку
     }
 
-    if (*str1 == '\0' && *str2 == '\0') {
-        return 0;
+    mergedSize = size1 + size2; // размер объединения
+    int* mergedArray = new int[mergedSize]; // новый массив
+    for (int i = 0; i < size1; ++i) {
+        mergedArray[i] = arr1[i];
     }
-    else if (*str1 == '\0') {
-        return -1;
+    for (int i = 0; i < size2; ++i) {
+        mergedArray[size1 + i] = arr2[i];
+    }
+    return mergedArray; 
+}
+
+int main() {
+    setlocale(LC_ALL, "");
+    int size1 = 3;
+    int size2 = 3;
+    int* arr1 = new int[size1] {1, 2, 3};
+    int* arr2 = new int[size2] {4, 5, 6};
+
+    int mergedSize;
+    int* mergedArray = mergeAndCompare(arr1, size1, arr2, size2, mergedSize);
+
+    if (mergedSize == -1) {
+        std::cout << "У массивов разные размеры" << std::endl;
     }
     else {
-        return 1;
-    }
-}
-
-int main() {
-    const char* str1 = "hello";
-    const char* str2 = "hello";
-    const char* str3 = "world";
-    const char* str4 = "hell";
-
-    cout << "Comparing \"" << str1 << "\" and \"" << str2 << "\": " << compareStrings(str1, str2) << endl;
-    cout << "Comparing \"" << str1 << "\" and \"" << str3 << "\": " << compareStrings(str1, str3) << endl;
-    cout << "Comparing \"" << str3 << "\" and \"" << str1 << "\": " << compareStrings(str3, str1) << endl;
-    cout << "Comparing \"" << str1 << "\" and \"" << str4 << "\": " << compareStrings(str1, str4) << endl;
-    cout << "Comparing \"" << str4 << "\" and \"" << str1 << "\": " << compareStrings(str4, str1) << endl;
-
-    return 0;
-}
-
-#include <iostream>
-
-// Функция для нахождения общих элементов в двух массивах
-int* findCommonElements(const int* arr1, int size1, const int* arr2, int size2, int& commonSize) {
-    int* commonElements = new int[size1 < size2 ? size1 : size2]; // Максимальный размер массива общих элементов
-    commonSize = 0;
-
-    for (int i = 0; i < size1; ++i) {
-        for (int j = 0; j < size2; ++j) {
-            if (arr1[i] == arr2[j]) {
-                commonElements[commonSize++] = arr1[i];
-                break; // Переходим к следующему элементу arr1
-            }
+        std::cout << "Объединенный массив: ";
+        for (int i = 0; i < mergedSize; ++i) {
+            std::cout << mergedArray[i] << " ";
         }
+        std::cout << std::endl;
     }
-
-    return commonElements;
-}
-
-int main() {
-    int arr1[] = { 1, 2, 3, 4, 5 };
-    int size1 = sizeof(arr1) / sizeof(arr1[0]);
-
-    int arr2[] = { 3, 4, 5, 6, 7 };
-    int size2 = sizeof(arr2) / sizeof(arr2[0]);
-
-    int commonSize;
-    int* commonElements = findCommonElements(arr1, size1, arr2, size2, commonSize);
-
-    std::cout << "Common elements: ";
-    for (int i = 0; i < commonSize; ++i) {
-        std::cout << commonElements[i] << " ";
+    delete[] arr1;
+    delete[] arr2;
+    if (mergedArray != nullptr) {
+        delete[] mergedArray;
     }
-    std::cout << std::endl;
-
-    delete[] commonElements; // Освобождение памяти
 
     return 0;
 }
